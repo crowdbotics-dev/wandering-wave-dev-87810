ed18e1b73d3ba8777c6efd09ccbcee5cab2f32e4
Crowdbotics <engineering@crowdbotics.com>
2023-06-01 13:51:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-wave-dev-87810",
+  url: "https://wandering-wave-dev-87810.botics.co",
+  api: "https://wandering-wave-dev-87810.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
